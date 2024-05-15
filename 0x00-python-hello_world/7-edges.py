19b6f20d5e3c39eeb8ce7b7e30e86bc5182d2b5c
Ryan <kryan2743@gmail.com>
2024-05-15 23:13:23 +0000

    anothrr

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def main():
+    word = "Holberton"
+    str = word
+    print(f"First 3 letters: {str[:3]}")
+    print(f"Last 2 letters: {str[7:]}")
+    print(f"Middle word: {str[1:8]}")
+main()
